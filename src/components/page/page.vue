2a1d4c4ce9e135ce1cf8c15ca2e908a0823de88a
yunser <>
2019-05-22 22:59:23 +0800

    backup

--- src/components/page/page.vue
+++ src/components/page/page.vue
@@ -6,10 +6,10 @@
             </div>
             <ui-list @itemClick="toggle()">
                 <ui-list-item title="首页" to="/">
-                    <ui-icon slot="left" value="grade"/>
+                    <ui-icon slot="left" value="home"/>
                 </ui-list-item>
-                <ui-list-item title="关于" to="/about">
-                    <ui-icon slot="left" value="grade"/>
+                <ui-list-item title="关于" href="https://project.yunser.com/products/afa08060684e11e98566ade6443a240e" target="_blank">
+                    <ui-icon slot="left" value="info"/>
                 </ui-list-item>
             </ui-list>
         </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
+/* eslint-disable */
 import Vue from 'vue'
 import Router from 'vue-router'
 
 const Home = resolve => require(['@/views/Home'], resolve)
 const Simple = resolve => require(['@/views/Simple'], resolve)
-const About = resolve => require(['@/views/About'], resolve)
 const ArticleDetail = resolve => require(['@/views/ArticleDetail'], resolve)
 const Error404 = resolve => require(['@/views/error/Error404'], resolve)
 
@@ -26,10 +26,6 @@ let routes = [
     {
         path: '/articles/:id',
         component: ArticleDetail
-    },
-    {
-        path: '/about',
-        component: About
     },
     {
         path: '*',
